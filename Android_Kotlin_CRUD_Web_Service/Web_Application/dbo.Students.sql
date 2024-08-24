65fd1775af3721c256608e936ed86f0021e849bb
Imran Javed <bsef21m033@pucit.edu.pk>
2024-08-24 14:22:58 +0500

    Create dbo.Students.sql

---
+++ Android_Kotlin_CRUD_Web_Service/Web_Application/dbo.Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Name]   NVARCHAR (100) NOT NULL,
+    [RollNo] NVARCHAR (50)  NOT NULL,
+    [CGPA]   FLOAT (53)     NOT NULL,
+    [Age]    INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([RollNo] ASC)
+);
+
